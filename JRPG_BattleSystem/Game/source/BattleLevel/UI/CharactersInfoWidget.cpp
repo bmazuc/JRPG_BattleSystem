d0030cbee01f4ee3ce85fd63ebd3d1f1f2240634
bmazuc <mazuc.bastien@gmail.com>
2026-08-11 10:34:32 +0200

    Remove useless class

--- JRPG_BattleSystem/Game/source/BattleLevel/UI/CharactersInfoWidget.cpp
+++
@@ -1,7 +0,0 @@
-#include "BattleLevel/UI/CharactersInfoWidget.h"
-#include "UI/Image.h"
-
-void CharactersInfoWidget::Construct()
-{
-	Image* background = CreateWidget<Image>("InfosBackground", UISpawnInfo(nullptr, TransformSpace::Local));
-}
